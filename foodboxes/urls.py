94e2a073539e317975a771fc537c8764ba5d6a33
Arif_Gafizov <agafizov@gmail.com>
2021-01-01 18:29:59 +0300

    Add path product-sets/ to urls.py

--- foodboxes/urls.py
+++ foodboxes/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from app_foodboxes.views import recipients
+from app_foodboxes.views import recipients, product_sets
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('recipients/', recipients, name='recipients'),
+    path('product-sets/', product_sets, name='product-sets'),
 ]
