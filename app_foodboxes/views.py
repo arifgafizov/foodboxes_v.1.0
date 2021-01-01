a3082799b325a72f7ad6b4475ec62380cc41ef02
Arif_Gafizov <agafizov@gmail.com>
2021-01-01 19:14:30 +0300

    Add recipient to views.py

--- app_foodboxes/views.py
+++ app_foodboxes/views.py
@@ -1,4 +1,5 @@
 import requests
+from rest_framework import status
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,6 +9,22 @@ def recipients(request):
     recipients = requests.get('https://stepik.org/media/attachments/course/73594/recipients.json')
     recipients_json = recipients.json()
     return Response(recipients_json)
+
+
+@api_view(http_method_names=['GET'])
+def recipient(request, pk):
+    print(pk, type(pk))
+    recipients = requests.get('https://stepik.org/media/attachments/course/73594/recipients.json')
+    recipients_json = recipients.json()
+    response = None
+    for recipient in recipients_json:
+        if recipient['id'] == pk:
+            response = recipient['info'], recipient['contacts']
+
+    if response:
+        return Response(response)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(http_method_names=['GET'])
