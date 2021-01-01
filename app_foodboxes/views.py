538413211f39f78b9ae6d6491d358b88b0ea16b4
Arif_Gafizov <agafizov@gmail.com>
2021-01-01 18:30:36 +0300

    Add product-sets to views.py

--- app_foodboxes/views.py
+++ app_foodboxes/views.py
@@ -8,3 +8,10 @@ def recipients(request):
     recipients = requests.get('https://stepik.org/media/attachments/course/73594/recipients.json')
     recipients_json = recipients.json()
     return Response(recipients_json)
+
+
+@api_view(http_method_names=['GET'])
+def product_sets(request):
+    foodboxes = requests.get('https://stepik.org/media/attachments/course/73594/foodboxes.json')
+    foodboxes_json = foodboxes.json()
+    return Response(foodboxes_json)
