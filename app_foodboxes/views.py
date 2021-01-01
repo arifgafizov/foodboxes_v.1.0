6b83adc10ee57eea8d5719ea30c02fc90f7394ea
Arif_Gafizov <agafizov@gmail.com>
2021-01-01 17:56:52 +0300

    Add recipients to views

--- app_foodboxes/views.py
+++ app_foodboxes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import requests
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(http_method_names=['GET'])
+def recipients(request):
+    recipients = requests.get('https://stepik.org/media/attachments/course/73594/recipients.json')
+    recipients_json = recipients.json()
+    return Response(recipients_json)
