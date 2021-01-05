f6102323ea6db2eb15771f390c376501aadc686a
Arif_Gafizov <agafizov@gmail.com>
2021-01-05 12:12:46 +0300

    Fixed mistakes pep8

--- app_foodboxes/views.py
+++ app_foodboxes/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view(http_method_names=['GET'])
 def recipients(request):
     recipients = requests.get('https://stepik.org/media/attachments/course/73594/recipients.json')
